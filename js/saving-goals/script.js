d7b13d581e3117f2279b462d82f52a3de0c989cb
Florian Lutz <florian.lutz@ymail.com>
2024-02-09 16:06:00 +0100

    solve challenge

---
+++ js/saving-goals/script.js
@@ -0,0 +1,13 @@
+let startingBalance = 1000;
+let monthlyContribution = 55;
+let months = 21;
+
+let totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After",
+  months,
+  "months of saving, you will have",
+  totalSavings,
+  "€ in your account."
+);
