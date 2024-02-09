2486b788af23a1e27c4427a477147d15cd329c63
Florian Lutz <florian.lutz@ymail.com>
2024-02-09 15:49:13 +0100

    solve challenge

---
+++ js/tip-calc/script.js
@@ -0,0 +1,8 @@
+let mealCost = 18;
+let tipPercentage = 12;
+
+let tipAmmount = (mealCost / 100) * tipPercentage;
+
+let totalCost = mealCost + tipAmmount;
+
+console.log("The total cost of your bill is:", totalCost, "euros.");
