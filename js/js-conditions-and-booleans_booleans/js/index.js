8dbf3183cc8cb80f99ee2c8976449ae606e36b1f
Florian Lutz <florian.lutz@ymail.com>
2024-02-12 15:18:06 +0100

    solve challenge

--- js/js-conditions-and-booleans_booleans/js/index.js
+++ js/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,41 @@
 console.clear();
 
-console.log(Boolean("Try it out"));
+console.log(Boolean(5 > 1));
+
+/*
+2 == "2"
+true
+
+2 === "2"
+false
+
+2 === 2
+true
+
+"hello world"
+true
+
+""
+false
+
+true
+true
+
+0
+false
+
+-50
+true
+
+50
+true
+
+5 > 1
+false
+
+"1 > 10"
+true
+
+"false"
+true
+*/
