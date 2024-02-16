cfabf266b9b6fa01b4228a13cf8a701418031167
Florian Lutz <florian.lutz@ymail.com>
2024-02-16 11:51:46 +0100

    solve challenge

--- js/js-forms_checkbox-input/js/index.js
+++ js/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,37 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccess() {
+  success.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+
+hideTosError();
+hideSuccess();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+  showSuccess();
+});
+
+tosCheckbox.addEventListener("input", (event) => {
+  if (event.target.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+  //or as ternary: event.target.checked ? hideTosError() : showTosError();
 });
