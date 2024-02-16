852f42f22305215fa9b1bd4077d74d98405d5d58
Florian Lutz <florian.lutz@ymail.com>
2024-02-16 11:18:25 +0100

    solve challenge

--- js/js-forms_handle-form-submit/js/index.js
+++ js/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const form = Object.fromEntries(formData);
+  console.log("Form:", form);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+  const sumAgeBadness = Number(form.age) + Number(form.badness);
+  console.log(`The age-badness-sum of ${form.firstName} is ${sumAgeBadness}`);
+});
