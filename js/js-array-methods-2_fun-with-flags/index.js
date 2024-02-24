f28dcc5d342e6cad5bcf64af223d5d8f4787b4a9
Florian Lutz <florian.lutz@ymail.com>
2024-02-24 21:26:58 +0100

    solve challenge

--- js/js-array-methods-2_fun-with-flags/index.js
+++ js/js-array-methods-2_fun-with-flags/index.js
@@ -9,7 +9,10 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountry = countries.find((country) =>
+    country.name.toLowerCase().startsWith(searchString.toLowerCase())
+  );
+  // country name and searchstring have been put to lowercase, so the search will not be case sensitive
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
