321c55c53110aa31c4136bacebbd85cb0d6e7348
Florian Lutz <florian.lutz@ymail.com>
2024-03-04 15:32:28 +0100

    solve challenge

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">This is an h2</h2>
+      <label htmlFor="name">Your Name</label>
+      <input type="text" id="name"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Sveti_Juraj"
+      >
+        Nice Article
+      </a>
+    </article>
+  );
 }
