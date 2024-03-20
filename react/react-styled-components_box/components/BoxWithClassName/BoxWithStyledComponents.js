c8a596c68093512349bb8fb8f76baeeabbeacb2d
Florian Lutz <florian.lutz@ymail.com>
2024-03-20 17:39:39 +0100

    change background-color

--- react/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
+++ react/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -3,16 +3,12 @@ import styled from "styled-components";
 const StyledBox = styled.div`
   width: 100px;
   height: 100px;
-  background-color: green;
   margin: 2rem;
+  background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
 
   &:hover {
     background-color: red;
   }
-  ${({ $isBlack }) =>
-    $isBlack &&
-    `background-color: black;
-    `}
 `;
 
 export default function BoxWithStyledComponents({ isBlack }) {
